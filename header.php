7a3ff39609ce488668dc4e0f50421728adc4cb8c
Lucy Park <me@lucypark.kr>
2013-07-06 16:33:04 +0900

    Add GitHub ribbon

--- header.php
+++ header.php
@@ -66,6 +66,7 @@
 </head>
 <body>
   <div id="banner"><img src="img/banner-beta-right-90.png"></div>
+  <a href="https://github.com/teampopong/nanow"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
   <div class="container">
   <div class="alert alert-error">
     <button type="button" class="close" data-dismiss="alert">&times;</button>
